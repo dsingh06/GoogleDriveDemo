002e0d068f233e1483b967fce2d17082c3c2b3fa
Damandroid <dsingh06@dcs.bbk.ac.uk>
2018-11-26 17:34:43 +0530

    Buttons hidden for now

--- app/src/main/java/com/thatapp/checklists/CheckListItemAdapter.kt
+++ app/src/main/java/com/thatapp/checklists/CheckListItemAdapter.kt
@@ -1,10 +1,9 @@
 package com.thatapp.checklists
 
-import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.R
 import android.content.Context
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
