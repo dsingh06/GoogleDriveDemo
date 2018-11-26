b8a0e7d0756bdceb563e6049cef0aa17b1e08409
Rishabh Awasthi <awasrishabh@gmail.com>
2018-11-26 18:55:57 +0530

    update @ 6.55

--- app/src/main/java/com/thatapp/checklists/CheckListItemAdapter.kt
+++ app/src/main/java/com/thatapp/checklists/CheckListItemAdapter.kt
@@ -3,44 +3,42 @@ package com.thatapp.checklists
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.R
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import kotlinx.android.synthetic.main.question_list.view.*
 
 
 class CheckListItemAdapter(var questionItemList:List<QuestionItem>,context:Context) :  RecyclerView.Adapter<CheckListItemAdapter.UserViewHolder>() {
 
     private  var context: Context = context
-
+   private var i = 1
     override fun getItemCount() = questionItemList.size
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
 
-        val inflatedView = LayoutInflater.from(context).inflate(R.layout.question_list,parent)
+        val inflatedView = LayoutInflater.from(context).inflate(R.layout.question_list,parent,false)
         return UserViewHolder(inflatedView)
     }
 
 
     override fun onBindViewHolder(holder: CheckListItemAdapter.UserViewHolder, position: Int) {
         val quesItem = questionItemList[position]
+
+        holder.tvSno.text = ""+i
+        holder.tvHeading.text = quesItem.strHeading
+        i++
+
     }
 
     inner class UserViewHolder (view: View) : RecyclerView.ViewHolder(view) {
 
-         var tvName: TextView? = null
-         var tvAddress: TextView? = null
-         var tvDate: TextView? = null
-         var tvConcernedPerson: TextView? = null
-         var tvMobile: TextView? = null
-        internal var btnReschedule: Button? = null
-        internal var btnCheckIn: Button? = null
-        internal var btnCheckOut: Button? = null
-         val layoutButtons: LinearLayout? = null
-         val itemLayout: LinearLayout? = null
+         val tvSno: TextView = view.tvQNo
+         var tvHeading: TextView = view.tvHeading
     }
 
 
