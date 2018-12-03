00d803c3dc0b0194e7e09970cd6fbbbc1f78bbab
Rishabh Awasthi <awasrishabh@gmail.com>
2018-12-03 15:23:48 +0530

    project update commit 3 DEC 2018 15.23

--- app/src/main/java/com/thatapp/checklists/ModelClasses/DisplayChecklistAndPDFAdapter.kt
+++ app/src/main/java/com/thatapp/checklists/ModelClasses/DisplayChecklistAndPDFAdapter.kt
@@ -1,5 +1,6 @@
 package com.thatapp.checklists.ModelClasses
 
+import android.app.Activity
 import android.support.v7.widget.RecyclerView
 
 import android.content.Context
@@ -7,13 +8,16 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.support.annotation.RequiresApi
+import android.support.v4.content.FileProvider
 import android.support.v7.view.menu.MenuPopupHelper
 import android.util.Log
 import android.view.*
 import android.widget.*
-import com.thatapp.checklists.ViewClasses.DisplayQuestionsActivity
 import com.thatapp.checklists.R
+import com.thatapp.checklists.ViewClasses.DisplayQuestionsActivity
 import kotlinx.android.synthetic.main.checklist_layout.view.*
+
+
 import okio.Okio
 import java.io.File
 import java.lang.Exception
@@ -21,12 +25,17 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 import kotlin.collections.ArrayList
+import com.thatapp.checklists.ViewClasses.MainActivity
+import android.support.v4.content.ContextCompat.startActivity
+import java.nio.file.Files.exists
+import android.support.v4.content.ContextCompat.startActivity
 
 
 class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context: Context, val type: String) : RecyclerView.Adapter<DisplayChecklistAndPDFAdapter.UserViewHolder>() {
 
     private val TAG = "MyDisplayAdapter"
-
+    private lateinit  var cacheFile: File
+    private var file_name = ""
     override fun getItemCount() = downloaded.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
@@ -40,8 +49,8 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
     }
 
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        lateinit var requestFile: File
 
         if (downloaded[position].name.contains(".pdf")) {
             val name: String = downloaded[position].name.substringBefore(":")
@@ -54,6 +63,56 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
                     .split("_")
             holder.updateDateTime.text = "Created: ".plus(timeCreated[1]).plus("   ").plus(timeCreated[0])
             Log.e(TAG, timeCreated[0] + " " + timeCreated[1])
+            holder.ivShare.setVisibility(View.VISIBLE)
+
+            holder.ivShare.setOnClickListener({
+                file_name = downloaded[position].name
+                share()
+
+                /*                 requestFile = File(context.filesDir.absolutePath + File.separator + "generated" + File.separator, downloaded[position].name)
+                           /*                val intent = Intent()
+                                           intent.action = Intent.ACTION_SEND
+                                           intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(requestFile))
+                                           intent.type = "application/pdf"
+                                           context.startActivity(intent)
+                             */
+                           //requestFile = File(downloaded[position].name)
+
+                         //  val requestFile = File(downloaded[position].name)
+
+                           //setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                           /*
+                        * Most file-related method calls need to be in
+                        * try-catch blocks.
+                        */
+                           lateinit var fileUri:Uri
+                           // Use the FileProvider to get a content URI
+                                         try {
+                                             fileUri = FileProvider.getUriForFile(
+                                                     context,
+                                                     "com.thatapp.checklists.fileprovider",
+                                                     requestFile)
+                                         } catch (e: Exception) {
+                                             Log.e("File Selector",
+                                                     "The selected file can't be shared: " + requestFile.toString()+"  "+e.toString())
+                                         }
+           //                Log.e("uri", Uri.fromFile(requestFile).toString())
+                           /* if (Uri.fromFile(requestFile) != null) {
+
+
+
+                                val mResultIntent: Intent = Intent()
+                                mResultIntent.setAction(Intent.ACTION_SEND)
+
+                                // Put the Uri and MIME type in the result Intent
+                                mResultIntent.setDataAndType(Uri.fromFile(requestFile), context.getContentResolver().getType(Uri.fromFile(requestFile)))
+                                Log.e("uri", Uri.fromFile(requestFile).toString())
+                                context.startActivity(mResultIntent)
+
+                            }*/
+                           // Log.e("uri",""+fileUri.toString())
+                  */
+            })
 
         } else {
             holder.fileName.text = downloaded[position].name.substringBefore(".")
@@ -61,13 +120,13 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
         }
 
 
+
         if (type.equals("xls")) {
 
             holder.parentView.setOnClickListener {
-                     val intent = Intent(context, DisplayQuestionsActivity::class.java)
-                     intent.putExtra("fileName", downloaded[position].name)
-                     context.startActivity(intent)
-
+                val intent = Intent(context, DisplayQuestionsActivity::class.java)
+                intent.putExtra("fileName", downloaded[position].name)
+                context.startActivity(intent)
 /*
                 val popup = PopupMenu(context, holder.parentView)
                 popup.inflate(R.menu.menu_popup)
@@ -111,6 +170,7 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
         var imageView: ImageView = view.ivFileImage
         var fileName: TextView = view.tvFileName
         var updateDateTime: TextView = view.tvDateTime
+        var ivShare: ImageView = view.ivShare
         var viewForeground = view.view_foreground
         var viewBackground = view.view_background
         var parentView: View
@@ -133,10 +193,10 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
         lateinit var dir1: File
         if (dir.equals("checklist")) {
             dir1 = File(context.filesDir.absolutePath + File.separator + "downloads")
-            Log.e("type","checklist")
-        } else  if (dir.equals("report")) {
+            Log.e("type", "checklist")
+        } else if (dir.equals("report")) {
             dir1 = File(context.filesDir.absolutePath + File.separator + "generated")
-            Log.e("type","report")
+            Log.e("type", "report")
         }
 
         if (dir1.isDirectory) {
@@ -176,10 +236,10 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
             lateinit var dir1: File
             if (dir.equals("checklist")) {
                 dir1 = File(context.filesDir.absolutePath + File.separator + "downloads")
-                Log.e("type","checklist")
-            } else  if (dir.equals("report")) {
+                Log.e("type", "checklist")
+            } else if (dir.equals("report")) {
                 dir1 = File(context.filesDir.absolutePath + File.separator + "generated")
-                Log.e("type","generated")
+                Log.e("type", "generated")
             }
 
             val tempFile = File(dir1, item.name)
@@ -208,5 +268,22 @@ class DisplayChecklistAndPDFAdapter(var downloaded: ArrayList<File>, var context
 
     }
 */
+
+
+    fun share() {
+
+        try {
+            cacheFile = File(context.filesDir.absolutePath + File.separator + "generated"+File.separator+file_name)
+            val uri = FileProvider.getUriForFile(context, context.getPackageName(), cacheFile)
+
+         val intent = Intent(Intent.ACTION_VIEW)
+         intent.data = uri
+         intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+         context.startActivity(intent)
+     }catch(e:Exception){
+            Log.e("err",e.toString())
+        }
+    }
+
 }
 
--- app/src/main/java/com/thatapp/checklists/ModelClasses/SectionModel.kt
+++
@@ -1,3 +0,0 @@
-package com.thatapp.checklists.ModelClasses
-
-class SectionModel(val sectionLabel: String, val itemArrayList: ArrayList<String>)
